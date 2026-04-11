7b602e5621319f8da71ebfb2135f18b8077531eb
gunhe_17 <pers.gun17@gmail.com>
2026-04-11 19:08:46 +0900

    lab

---
+++ architecture/domain/user/user_repository.py
@@ -0,0 +1,60 @@
+from abc import abstractmethod
+
+from architecture.core.repository import Repository
+
+from architecture.domain.user.user import User
+from architecture.domain.user.email import Email
+
+
+class UserRepository(Repository):
+    # #
+    # create
+
+    @abstractmethod
+    def add(self, entity: User) -> None:
+        ...
+
+    @abstractmethod
+    def add_many(self, entities: list[User]) -> None:
+        ...
+
+    # #
+    # read
+
+    @abstractmethod
+    def get(self, id: str) -> User | None:
+        ...
+
+    @abstractmethod
+    def get_many(self, ids: list[str]) -> list[User]:
+        ...
+
+    @abstractmethod
+    def exists(self, id: str) -> bool:
+        ...
+
+    @abstractmethod
+    def find_by_email(self, email: Email) -> User | None:
+        ...
+
+    # #
+    # update
+
+    @abstractmethod
+    def update(self, entity: User) -> None:
+        ...
+
+    @abstractmethod
+    def update_many(self, entities: list[User]) -> None:
+        ...
+
+    # #
+    # delete
+
+    @abstractmethod
+    def remove(self, id: str) -> None:
+        ...
+
+    @abstractmethod
+    def remove_many(self, ids: list[str]) -> None:
+        ...
